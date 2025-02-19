07f48885361ac1edb04ed05b72c6f32c377f7c96
jmarquezba <124936850+jmarquezba@users.noreply.github.com>
2025-02-19 14:11:53 -0500

    Lab6

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,6 +1,9 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
+import pandas as pd
+from datetime import datetime
+import re
 
 
 def pregunta_01():
@@ -13,3 +16,39 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    file = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";", index_col=0)
+    df = file.copy()
+    
+    df = df.dropna()
+    
+    df["sexo"] = df["sexo"].str.lower()
+
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+    df["barrio"] = df["barrio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+    df["estrato"] = df["estrato"].astype(int)
+
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    df["fecha_de_beneficio"] = [
+        (
+            datetime.strptime(date, "%d/%m/%Y")
+            if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", date))
+            else datetime.strptime(date, "%Y/%m/%d")
+        )
+
+        for date in df["fecha_de_beneficio"]
+        ]
+    
+    df["monto_del_credito"] = df["monto_del_credito"].str.strip("$").str.replace(r"\.00", "", regex=True).str.replace(",","").astype(int)
+    df["línea_credito"] = df["línea_credito"].str.strip().str.lower().str.replace("-", " ").str.replace("_", " ").str.replace(". ", ".")
+    
+    df = df.drop_duplicates()
+    
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=";", index=True)
+
+
+pregunta_01()
